e5e2fc140311bec0adf563d1b559c22cab4f9d0a
한결 <gyeol@hangyeol-ui-iMac.local>
2021-01-07 09:44:18 +0900

    ex00 fix

--- module00/ex00/megaphone.cpp
+++ module00/ex00/megaphone.cpp
@@ -1,10 +1,5 @@
 #include <iostream>
 
-void	ft_putstr(const char *str)
-{
-	std::cout << str;
-}
-
 int		megaphone(char **argv)
 {
 	int		i;
@@ -17,20 +12,22 @@ int		megaphone(char **argv)
 		while (argv[i][j])
 		{
 			if (argv[i][j] >= 'a' && argv[i][j] <= 'z')
-				std::cout << argv[i][j] + ('A' - 'a');
+				std::cout << (char)(argv[i][j] - 32);
 			else
 				std::cout << argv[i][j];
 			j++;
 		}
 		i++;
 	}
+	return (0);
 }
 
 int		main(int argc, char **argv)
 {
 	if (argc == 1)
-		ft_putstr("* LOUD AND UNBEARABLE FEEDBACK NOISE *\n");
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *";
 	else
 		megaphone(argv);
+	std::cout << "\n";
 	return (0);
 }
